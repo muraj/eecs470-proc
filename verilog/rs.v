3f3e796ba681848d7dac9ba172c9a7707159a8b7
yejoong <yejoong@035cc615-df9f-21d4-9745-e54a158c10d1>
2011-03-12 03:52:38 +0000

    RS for SuperScalar has been updated. But not yet tested.

--- verilog/rs.v
+++ verilog/rs.v
@@ -2,13 +2,14 @@ module RS(clk, reset,
                 //INPUTS
                 rs_en, prega_idx, pregb_idx, pdest_idx, prega_valid, pregb_valid, //RAT
                 ALUop, rd_mem, wr_mem, rs_IR,  npc, cond_branch, uncond_branch, //Issue Stage
-                mult_free, ALU_free, mem_free, cdb_valid, cdb_tag, //Pipeline communication
+                mult_free, ALU_free, mem_free, cdb_valid, cdb_tag, entry_flush,//Pipeline communication
                 rob_idx,  //ROB
 
                 //OUTPUT
                 rs_free,  ALU_rdy, mem_rdy, mult_rdy, //Hazard detect
                 pdest_idx_out, prega_idx_out, pregb_idx_out, ALUop_out, rd_mem_out,   //FU
-                wr_mem_out, rs_IR_out, npc_out, rob_idx_out                           //FU
+                wr_mem_out, rs_IR_out, npc_out, rob_idx_out,                           //FU
+								rs_idx_out																													// ROB
           );
 
   input wire  clk, reset;
@@ -23,15 +24,18 @@ module RS(clk, reset,
   input wire  mult_free, ALU_free, mem_free;
 	input wire	[`SCALAR-1:0] cdb_valid;
   input wire  [`SCALAR-1:0] cdb_tag [`PRF_IDX-1:0];
+	input wire	[`SCALAR-1:0] entry_flush [`RS_SZ-1:0];
   input wire  [`ROB_IDX-1:0] rob_idx;
 
-  output wire rs_free, ALU_rdy, mem_rdy, mult_rdy;
+  output wire rs_free;
+	output wire ALU_rdy, mem_rdy, mult_rdy;
   output wire [`PRF_IDX-1:0] pdest_idx_out, prega_idx_out, pregb_idx_out;
   output wire [4:0] ALUop_out;
   output wire  rd_mem_out, wr_mem_out;
   output wire [31:0] rs_IR_out;
   output wire [63:0] npc_out;
   output wire [`ROB_IDX-1:0] rob_idx_out;
+	output wire [`RS_IDX-1:0] rs_idx_out; // Output of ex encoder
 
   wire [`RS_SZ-1:0] entry_free;
   wire [`RS_SZ-1:0] entry_ALU_rdy, entry_mem_rdy, entry_mult_rdy;
@@ -45,21 +49,24 @@ module RS(clk, reset,
   wire [63:0] npc_int [`RS_SZ-1:0]; 
   wire [`ROB_IDX-1:0] rob_idx_int [`RS_SZ-1:0];
 
-  wire [`RS_IDX-1:0] entry_idx; // Output of ex encoder
   wire [`RS_SZ-1:0] entry_en; // Output of issue selector
   wire [`RS_SZ-1:0] entry_ALU_sel, entry_mem_sel, entry_mult_sel; // Output of the encoders (ALU_sel, mem_sel, mult_sel)
 	wire [`RS_SZ-1:0] entry_sel; // selected entry for execution
 
-  assign pdest_idx_out = pdest_idx_int[entry_idx];
-  assign prega_idx_out = prega_idx_int[entry_idx];
-  assign pregb_idx_out = pregb_idx_int[entry_idx];
-  assign ALUop_out = ALUop_int[entry_idx];
-  assign rd_mem_out = rd_mem_int[entry_idx];
-  assign wr_mem_out = wr_mem_int[entry_idx];
-  assign rs_IR_out = rs_IR_int[entry_idx];
-  assign npc_out = npc_int[entry_idx];
-  assign rob_idx_out = rob_idx_int[entry_idx];
+  assign pdest_idx_out = pdest_idx_int[rs_idx_out];
+  assign prega_idx_out = prega_idx_int[rs_idx_out];
+  assign pregb_idx_out = pregb_idx_int[rs_idx_out];
+  assign ALUop_out = ALUop_int[rs_idx_out];
+  assign rd_mem_out = rd_mem_int[rs_idx_out];
+  assign wr_mem_out = wr_mem_int[rs_idx_out];
+  assign rs_IR_out = rs_IR_int[rs_idx_out];
+  assign npc_out = npc_int[rs_idx_out];
+  assign rob_idx_out = rob_idx_int[rs_idx_out];
   assign rs_free = | entry_free;
+
+	assign ALU_rdy = | entry_ALU_rdy;
+	assign mem_rdy = | entry_mem_rdy;
+	assign mult_rdy = | entry_mult_rdy;
 
 	assign entry_sel = (ALU_free) ? entry_ALU_sel :
 										 ((mem_free) ? entry_mem_sel :
@@ -70,7 +77,7 @@ ps #(.NUM_BITS(`RS_SZ)) issue_sel(.req(entry_free), .en(rs_en), .gnt(entry_en), 
 ps #(.NUM_BITS(`RS_SZ)) ALU_sel(.req(entry_ALU_rdy), .en(ALU_free), .gnt(entry_ALU_sel), .req_up());
 ps #(.NUM_BITS(`RS_SZ)) mem_sel(.req(entry_mem_rdy), .en(mem_free), .gnt(entry_mem_sel), .req_up());
 ps #(.NUM_BITS(`RS_SZ)) mult_sel(.req(entry_mult_rdy), .en(mult_free), .gnt(entry_mult_sel), .req_up());
-pe #(.OUT_WIDTH(`RS_IDX)) ex_encode(.gnt(entry_sel), .enc(entry_idx)); 
+pe #(.OUT_WIDTH(`RS_IDX)) ex_encode(.gnt(entry_sel), .enc(rs_idx_out)); 
 
 
 
@@ -78,7 +85,7 @@ generate
   genvar i;
   for(i=0; i<`RS_SZ; i=i+1) begin : entries
     rs_entry entry ( .clk(clk),
-                                  .reset(reset),
+                                  .reset(reset | entry_flush[i]),
                           //INPUTS
                                   .entry_en(entry_en[i]),  //ISSUE_SEL 
                                   .entry_sel(entry_sel[i]), //EX_SEL
