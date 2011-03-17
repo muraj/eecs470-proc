a13a16b1f13165107e9ccfc11a8837d92895f090
gyouho <gyouho@035cc615-df9f-21d4-9745-e54a158c10d1>
2011-03-17 04:30:32 +0000

    first draft of rob is finished, now need to test it

--- verilog/rob.v
+++ verilog/rob.v
@@ -3,16 +3,18 @@ module rob (clk, reset,
 						din1_rdy, din2_rdy, dout1_valid, dout2_valid,  
 						// allocate requests
 						din1_req, din2_req,
+						// update requests
+						up1_req, up2_req,
 						// incoming values
-						ir_in1, ir_in2, npc_in1, npc_in2, pdest_in1, pdest_in2,
-						// values that need updates
-						btaken_in1, btaken_in2, bt_addr_in1, bt_addr_in2, 
+						ir_in1, ir_in2, npc_in1, npc_in2, pdest_in1, pdest_in2, ba_pd_in1, ba_pd_in2, bt_pd_in1, bt_pd_in2,
+						// values that gets updated
+						ba_ex_in1, ba_ex_in2, bt_ex_in1, bt_ex_in2, bt_addr_in1, bt_addr_in2, 
 						// rob indices for updates
 						rob_idx_in1, rob_idx_in2,
 						// allocated rob indices
 						rob_idx_out1, rob_idx_out2,
-						// destination PRF index for retiring
-						pdest_out1, pdest_out2
+						// output values at retirement
+						ir_out1, ir_out2, npc_out1, npc_out2, pdest_out1, pdest_out2
 						);
 
   input clk, reset, din1_req, din2_req;
@@ -23,29 +25,54 @@ module rob (clk, reset,
 	input [63:0] bt_addr_in1, bt_addr_in2;
 	input [`ROB_IDX-1:0] rob_idx_in1, rob_idx_in2;
 	
-	output reg din1_rdy, din2_rdy, dout1_valid, dout2_valid;
+	output din1_rdy, din2_rdy;
+	output reg dout1_valid, dout2_valid;
 	output [`ROB_IDX-1:0] rob_idx_out1, rob_idx_out2;
 	output [`PRF_IDX-1:0] pdest_out1, pdest_out2;
+	output [31:0] ir_out1, ir_out2;
+	output [63:0] npc_out1, npc_out2;
 
-  // FIXME from here
 
-	// internal regs
-	reg [63-1:0] data_bt_addr [`ROB_SZ-1:0];
-	reg [`ROB_SZ-1:0] data_btaken;
+  // Data input indicators for outside world
+	assign din1_rdy = !full;
+	assign din2_rdy = !full || !full_almost;
+
+	assign rob_idx_out1 = tail;
+	assign rob_idx_out2 = tail_p1;
+
+
+	// Retiring decision
+	wire retire1, retire2;
+
+	assign retire1 = !empty && data_rdy[head];
+	assign retire2 = !empty_almost && retire1 && data_rdy[head_p1];
+	
+	// Branch miss
+	reg branch_miss, move_tail;
+	reg tail_new;
+
+	// ===================================================
+	// Duplicate cb functionality for things to be updated
+	// ===================================================
+	reg [63-1:0] 			data_ba_ex [`ROB_SZ-1:0];
+	reg [`ROB_SZ-1:0] data_bt_ex;
 	reg [`ROB_SZ-1:0] data_rdy;
-	reg [63-1:0] data_bt_addr1 [`ROB_SZ-1:0];
 
-	reg [CB_WIDTH-1:0] next_data1, next_data2;
-	reg [CB_IDX-1:0] next_head, next_tail;
+	reg [63-1:0] next_data_ba_ex1, next_data_ba_ex2;
+	reg next_data_bt_ex1, next_data_bt_ex2;
+	reg next_data_rdy1, next_data_rdy2;
+
+
+	reg [`ROB_IDX-1:0] next_head, next_tail;
 
 	// i/o counter
-	reg [CB_IDX:0] iocount;
-	wire [CB_IDX:0] next_iocount;
+	reg [`ROB_IDX:0] iocount;
+	wire [`ROB_IDX:0] next_iocount;
 	reg [1:0] incount, outcount;
 	reg empty, empty_almost;
 
 	// purely combinational
-	wire [CB_IDX-1:0] tail_p1, tail_p2, head_p1, head_p2, cur_size;
+	wire [`ROB_IDX-1:0] tail_p1, tail_p2, head_p1, head_p2, cur_size;
 	wire next_full, next_full_almost, next_empty, next_empty_almost;
 	
 	assign tail_p1 = tail + 1'd1;
@@ -53,63 +80,122 @@ module rob (clk, reset,
 	assign head_p1 = head + 1'd1;
 	assign head_p2 = head + 2'd2;
 	
-	assign cur_size = (next_tail>=next_head)? (next_tail - next_head) : (next_tail + CB_LENGTH - next_head);
+	assign cur_size = (next_tail>=next_head)? (next_tail - next_head) : (next_tail + `ROB_SZ - next_head);
 	assign next_iocount = (move_tail)? cur_size : iocount + incount - outcount;
-	assign next_full = next_iocount == CB_LENGTH;
-	assign next_full_almost = next_iocount == (CB_LENGTH-1);
+	assign next_full = next_iocount == `ROB_SZ;
+	assign next_full_almost = next_iocount == (`ROB_SZ-1);
 	assign next_empty = next_iocount == 0;
 	assign next_empty_almost = next_iocount == 1;
 
 	always @* begin
-		// default cases
-		next_data1 = data[tail];
-		next_data2 = data[tail_p1];
+		// default cases for data
+		next_data_ba_ex1 = data_ba_ex[tail];
+		next_data_ba_ex2 = data_ba_ex[tail_p1];
+		next_data_bt_ex1 = data_bt_ex[tail];
+		next_data_bt_ex2 = data_bt_ex[tail_p1];
+		next_data_rdy1 = data_rdy[tail];
+		next_data_rdy2 = data_rdy[tail_p1];
+		
+		// other default cases
 		next_head = head;
 		next_tail = tail;
 		incount = 2'd0;
 		outcount = 2'd0;
+		dout1_valid = retire1;
+		dout2_valid = retire2;
+		branch_miss = 0;
 
-		// deal with tail and data in
+		// deal with branch misses
+		if (retire1) begin
+			if ((data_bt_ex[head] != bt_pd_out1) || (data_ba_ex[head] != ba_pd_out1)) begin
+				branch_miss = 1;
+				dout2_valid = 0;	
+				move_tail = 1;
+				tail_new = next_head;
+			end else if (retire2) begin
+				if ((data_bt_ex[head_p1] != bt_pd_out2) || (data_ba_ex[head_p1] != ba_pd_out2)) begin
+					branch_miss = 1;
+					move_tail = 1;
+					tail_new = next_head;
+				end 
+			end
+		end
+
+
+		// deal with tail and data in (allocate)
 		if (move_tail) begin
 			next_tail = tail_new;
 		end else begin
-			if (din1_en && !full) begin
+			if (din1_req && !full) begin
 				next_tail = tail_p1;
-				next_data1 = din1;
 				incount = 2'd1;
-				if (din2_en && !full_almost) begin
+
+				next_data_ba_ex1 = {64{1'b0}};
+				next_data_bt_ex1 = 1'b0;
+				next_data_rdy1 = 1'b0;
+
+				if (din2_req && !full_almost) begin
 					next_tail = tail_p2;
-					next_data2 = din2;
 					incount = 2'd2;
+					
+					next_data_ba_ex2 = {64{1'b0}};
+					next_data_bt_ex2 = 1'b0;
+					next_data_rdy2 = 1'b0;
+
 				end
 			end
 		end
 
 		// deal with head and data out
-		if (dout1_req && !empty) begin
+		if (retire1 && !empty) begin
 			next_head = head_p1;
-			dout1 = data[head];
+			//dout1 = data[head];
 			outcount = 2'd1;
-			if (dout2_req && !empty_almost) begin
+			if (retire2 && !empty_almost) begin
 				next_head = head_p2;
-				dout2 = data[head_p1];
+				//dout2 = data[head_p1];
 				outcount = 2'd2;
 			end
 		end
-	end
 
+
+	end // always @*
+
+
+	// ===================================================
+	// Sequential Block
+	// ===================================================
 	always @(posedge clk) begin
 		if (reset) begin
-			head 					<= `SD {CB_IDX{1'b0}};
-			tail 					<= `SD {CB_IDX{1'b0}};
-			iocount 			<= `SD {CB_IDX+1{1'b0}};
+			head 					<= `SD {`ROB_IDX{1'b0}};
+			tail 					<= `SD {`ROB_IDX{1'b0}};
+			iocount 			<= `SD {`ROB_IDX+1{1'b0}};
 			full 					<= `SD 1'b0;
 			full_almost 	<= `SD 1'b0;
 			empty					<= `SD 1'b0;
 			empty_almost	<= `SD 1'b0;
+
 		end else begin
-			data[tail]		<= `SD next_data1;
-			data[tail_p1] <= `SD next_data2;
+			// data allocation
+			data_ba_ex[tail]			<= `SD next_data_ba_ex1;
+			data_ba_ex[tail_p1]		<= `SD next_data_ba_ex2;
+			data_bt_ex[tail]			<= `SD next_data_bt_ex1;
+			data_bt_ex[tail_p1]		<= `SD next_data_bt_ex2;
+			data_rdy[tail]				<= `SD next_data_rdy1;
+			data_rdy[tail_p1]			<= `SD next_data_rdy2;
+
+			// data updates
+			if (up1_req) begin
+				data_ba_ex[rob_idx_in1] <= `SD ba_ex_in1;
+				data_bt_ex[rob_idx_in1] 	<= `SD bt_ex_in1;
+				data_rdy[rob_idx_in1] 		<= `SD 1'b1;
+				if (up2_req) begin
+					data_ba_ex[rob_idx_in2] <= `SD ba_ex_in2;
+					data_bt_ex[rob_idx_in2] 	<= `SD bt_ex_in2;
+					data_rdy[rob_idx_in2] 		<= `SD 1'b1;
+				end
+			end
+
 			head 					<= `SD next_head;
 			tail					<= `SD next_tail;
 			iocount 			<= `SD next_iocount;
@@ -120,26 +206,43 @@ module rob (clk, reset,
 		end
 	end
 
+	// ===================================================
+	// Circular buffers for entries not awaiting updates
+	// ===================================================
 
 	// Circular buffer for IR
 	cb #(.CB_IDX(`ROB_IDX), .CB_WIDTH(32), .CB_LENGTH(`ROB_SZ)) cb_ir (
 		.clk(clk), .reset(reset),	.move_tail(move_tail), .tail_new(tail_new), 
-		.din1_en(wr1), .din2_en(wr2), .dout1_req(rd1), .dout2_req(rd2),
-		.din1(din_ir1), .din2(din_ir2), .dout1(dout_ir1), .dout2(dout_ir2), 
+		.din1_en(din1_req), .din2_en(din2_req), .dout1_req(retire1), .dout2_req(retire2),
+		.din1(ir_in1), .din2(ir_in2), .dout1(ir_out1), .dout2(ir_out2), 
 		.full(), .full_almost(), .head(), .tail());
 
 	// Circular buffer for NPC
 	cb #(.CB_IDX(`ROB_IDX), .CB_WIDTH(64), .CB_LENGTH(`ROB_SZ)) cb_npc (
 		.clk(clk), .reset(reset),	.move_tail(move_tail), .tail_new(tail_new), 
-		.din1_en(wr1), .din2_en(wr2), .dout1_req(rd1), .dout2_req(rd2),
-		.din1(din_npc1), .din2(din_npc2), .dout1(dout_npc1), .dout2(dout_npc2), 
+		.din1_en(din1_req), .din2_en(din2_req), .dout1_req(retire1), .dout2_req(retire2),
+		.din1(npc_in1), .din2(npc_in2), .dout1(npc_out1), .dout2(npc_out2), 
 		.full(), .full_almost(), .head(), .tail());
 
 	// Circular buffer for PDEST_IDX
 	cb #(.CB_IDX(`ROB_IDX), .CB_WIDTH(32), .CB_LENGTH(`ROB_SZ)) cb_pdest (
 		.clk(clk), .reset(reset),	.move_tail(move_tail), .tail_new(tail_new), 
-		.din1_en(wr1), .din2_en(wr2), .dout1_req(rd1), .dout2_req(rd2),
-		.din1(din_pdest1), .din2(din_pdest2), .dout1(dout_pdest1), .dout2(dout_pdest2), 
+		.din1_en(din1_req), .din2_en(din2_req), .dout1_req(retire1), .dout2_req(retire2),
+		.din1(pdest_in1), .din2(pdest_in2), .dout1(pdest_out1), .dout2(pdest_out2), 
+		.full(), .full_almost(), .head(), .tail());
+
+	// Circular buffer for Predicted Branch Address
+	cb #(.CB_IDX(`ROB_IDX), .CB_WIDTH(64), .CB_LENGTH(`ROB_SZ)) cb_ba_pd (
+		.clk(clk), .reset(reset),	.move_tail(move_tail), .tail_new(tail_new), 
+		.din1_en(din1_req), .din2_en(din2_req), .dout1_req(retire1), .dout2_req(retire2),
+		.din1(ba_pd_in1), .din2(ba_pd_in2), .dout1(ba_pd_out1), .dout2(ba_pd_out2), 
+		.full(), .full_almost(), .head(), .tail());
+
+	// Circular buffer for Predicted Branch Direction
+	cb #(.CB_IDX(`ROB_IDX), .CB_WIDTH(1), .CB_LENGTH(`ROB_SZ)) cb_bt_pd (
+		.clk(clk), .reset(reset),	.move_tail(move_tail), .tail_new(tail_new), 
+		.din1_en(din1_req), .din2_en(din2_req), .dout1_req(retire1), .dout2_req(retire2),
+		.din1(bt_pd_in1), .din2(bt_pd_in2), .dout1(bt_pd_out1), .dout2(bt_pd_out2), 
 		.full(), .full_almost(), .head(), .tail());
 
 endmodule
