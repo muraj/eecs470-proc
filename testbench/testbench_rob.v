3b6408ac2702bff6b3b309e369e86570c54da497
gyouho <gyouho@035cc615-df9f-21d4-9745-e54a158c10d1>
2011-03-19 02:50:14 +0000

    finished setting up rob testbench

--- testbench/testbench_rob.v
+++ testbench/testbench_rob.v
@@ -7,14 +7,17 @@
 
 module testbench;
 
-  integer count,limbo,idx;  //TEST VARS
+  integer count, limbo, idx, NPC;  //TEST VARS
 
-  reg clk, reset, din1_req, din2_req, up1_req, up2_req;
+  reg clk, reset, din1_req, din2_req, dup1_req, dup2_req;
 	reg [31:0] ir_in1, ir_in2;
 	reg [63:0] npc_in1, npc_in2;
 	reg [`PRF_IDX-1:0] pdest_in1, pdest_in2;
-	reg btaken_in1, btaken_in2;
-	reg [63:0] bt_addr_in1, bt_addr_in2;
+	reg bt_pd_in1, bt_pd_in2;
+	reg [63:0] ba_pd_in1, ba_pd_in2;
+	reg isbranch_in1, isbranch_in2;
+	reg bt_ex_in1, bt_ex_in2;
+	reg [63:0] ba_ex_in1, ba_ex_in2;
 	reg [`ROB_IDX-1:0] rob_idx_in1, rob_idx_in2;
 	
 	wire din1_rdy, din2_rdy;
@@ -29,9 +32,9 @@ module testbench;
 	rob rob0 (clk, reset, 
 						din1_rdy, din2_rdy, dout1_valid, dout2_valid,  
 						din1_req, din2_req,
-						up1_req, up2_req,
-						ir_in1, ir_in2, npc_in1, npc_in2, pdest_in1, pdest_in2, ba_pd_in1, ba_pd_in2, bt_pd_in1, bt_pd_in2,
-						ba_ex_in1, ba_ex_in2, bt_ex_in1, bt_ex_in2, bt_addr_in1, bt_addr_in2, 
+						dup1_req, dup2_req,
+						ir_in1, ir_in2, npc_in1, npc_in2, pdest_in1, pdest_in2, ba_pd_in1, ba_pd_in2, bt_pd_in1, bt_pd_in2, isbranch_in1, isbranch_in2,
+						ba_ex_in1, ba_ex_in2, bt_ex_in1, bt_ex_in2, 
 						rob_idx_in1, rob_idx_in2,
 						rob_idx_out1, rob_idx_out2,
 						ir_out1, ir_out2, npc_out1, npc_out2, pdest_out1, pdest_out2,
@@ -44,7 +47,7 @@ module testbench;
     clk = ~clk;
   end
 
-
+/*
   always @(posedge clk) //simulating 
   begin
      #2;
@@ -61,26 +64,25 @@ module testbench;
 				$finish;
 			end 
 	end
+*/
 
-  task show_IO_content;
+  task show_io;
 	  begin
 		
-    $display("============================================================================================ ");
-    $display("Din1 | Den1 | Din2 | Den2 | Dout1 | Dreq1 | Dout2 | Dreq2 | move tail | move offset ");
-    $display("============================================================================================ ");
+    $display("==OUTPUTS====================================================");
+   	$display("RDY1\tRDY2\tROB1\tROB2\tVAL1\tVAL2\tMISS\tBA");
+		$display("%b\t%b\t%0d\t%0d\t%b\t%b\t%b\t%0d", din1_rdy, din2_rdy, rob_idx_out1, rob_idx_out2, dout1_valid, dout2_valid, branch_miss, ba_out);
+    $display("=============================================================\n");
 
-    $display("0x%h |  %d   | 0x%h |  %d   | 0x%h  |  %d    | 0x%h  |  %d    |    %b      | 0x%h ",din1, din1_en, din2, din2_en, dout1, dout1_req, dout2, dout2_req, move_tail, tail_new);
-    $display("============================================================================================ ");
 	  end
 	endtask
 
 
-	task show_entry_content;
+	task show_contents;
 	  begin
-		
-		$display("\n========================== ");
-    $display("ROB Contents:");
-    $display("========================== ");
+		$display("==============================================================");
+    $display("ROB Contents");
+		$display("==============================================================");
 
     $display("Counter : %d",rob0.iocount);
 		$display("Full  / Almost : %0d,%0d",rob0.full, rob0.full_almost);
@@ -88,231 +90,164 @@ module testbench;
     $display("Head : %d",rob0.head);
 		$display("Tail : %d\n",rob0.tail);
     
-    $display("Entry 0 | %d\t\t%",rob0.data_rdy[0], rob0.data_bt_ex[0], rob0.data_ba_ex[0], rob0.cb_npc.data[0], rob0.cb_pdest.data[0], rob0.cb_bt_pd.data[0], rob0.cb_ba_pd.data[0]);
+		$display("         |  RDY\tBTEX\tBAEX\tNPC\tPDEST\tBTPD\tBAPD");
+		$display("==============================================================");
+    $display("Entry  0 |  %b\t%b\t%0d\t%0d\t%0d\t%b\t%0d",rob0.data_rdy[0], rob0.data_bt_ex[0], rob0.data_ba_ex[0], rob0.cb_npc.data[0], rob0.cb_pdest.data[0], rob0.cb_bt_pd.data[0], rob0.cb_ba_pd.data[0]);
+    $display("Entry  1 |  %b\t%b\t%0d\t%0d\t%0d\t%b\t%0d",rob0.data_rdy[1], rob0.data_bt_ex[1], rob0.data_ba_ex[1], rob0.cb_npc.data[1], rob0.cb_pdest.data[1], rob0.cb_bt_pd.data[1], rob0.cb_ba_pd.data[1]);
+    $display("Entry  2 |  %b\t%b\t%0d\t%0d\t%0d\t%b\t%0d",rob0.data_rdy[2], rob0.data_bt_ex[2], rob0.data_ba_ex[2], rob0.cb_npc.data[2], rob0.cb_pdest.data[2], rob0.cb_bt_pd.data[2], rob0.cb_ba_pd.data[2]);
+    $display("Entry  3 |  %b\t%b\t%0d\t%0d\t%0d\t%b\t%0d",rob0.data_rdy[3], rob0.data_bt_ex[3], rob0.data_ba_ex[3], rob0.cb_npc.data[3], rob0.cb_pdest.data[3], rob0.cb_bt_pd.data[3], rob0.cb_ba_pd.data[3]);
+    $display("Entry  4 |  %b\t%b\t%0d\t%0d\t%0d\t%b\t%0d",rob0.data_rdy[4], rob0.data_bt_ex[4], rob0.data_ba_ex[4], rob0.cb_npc.data[4], rob0.cb_pdest.data[4], rob0.cb_bt_pd.data[4], rob0.cb_ba_pd.data[4]);
+    $display("Entry  5 |  %b\t%b\t%0d\t%0d\t%0d\t%b\t%0d",rob0.data_rdy[5], rob0.data_bt_ex[5], rob0.data_ba_ex[5], rob0.cb_npc.data[5], rob0.cb_pdest.data[5], rob0.cb_bt_pd.data[5], rob0.cb_ba_pd.data[5]);
+    $display("Entry  6 |  %b\t%b\t%0d\t%0d\t%0d\t%b\t%0d",rob0.data_rdy[6], rob0.data_bt_ex[6], rob0.data_ba_ex[6], rob0.cb_npc.data[6], rob0.cb_pdest.data[6], rob0.cb_bt_pd.data[6], rob0.cb_ba_pd.data[6]);
+    $display("Entry  7 |  %b\t%b\t%0d\t%0d\t%0d\t%b\t%0d",rob0.data_rdy[7], rob0.data_bt_ex[7], rob0.data_ba_ex[7], rob0.cb_npc.data[7], rob0.cb_pdest.data[7], rob0.cb_bt_pd.data[7], rob0.cb_ba_pd.data[7]);
+    $display("Entry  8 |  %b\t%b\t%0d\t%0d\t%0d\t%b\t%0d",rob0.data_rdy[8], rob0.data_bt_ex[8], rob0.data_ba_ex[8], rob0.cb_npc.data[8], rob0.cb_pdest.data[8], rob0.cb_bt_pd.data[8], rob0.cb_ba_pd.data[8]);
+    $display("Entry  9 |  %b\t%b\t%0d\t%0d\t%0d\t%b\t%0d",rob0.data_rdy[9], rob0.data_bt_ex[9], rob0.data_ba_ex[9], rob0.cb_npc.data[9], rob0.cb_pdest.data[9], rob0.cb_bt_pd.data[9], rob0.cb_ba_pd.data[9]);
+    $display("Entry 10 |  %b\t%b\t%0d\t%0d\t%0d\t%b\t%0d",rob0.data_rdy[10], rob0.data_bt_ex[10], rob0.data_ba_ex[10], rob0.cb_npc.data[10], rob0.cb_pdest.data[10], rob0.cb_bt_pd.data[10], rob0.cb_ba_pd.data[10]);
+    $display("Entry 11 |  %b\t%b\t%0d\t%0d\t%0d\t%b\t%0d",rob0.data_rdy[11], rob0.data_bt_ex[11], rob0.data_ba_ex[11], rob0.cb_npc.data[11], rob0.cb_pdest.data[11], rob0.cb_bt_pd.data[11], rob0.cb_ba_pd.data[11]);
+    $display("Entry 12 |  %b\t%b\t%0d\t%0d\t%0d\t%b\t%0d",rob0.data_rdy[12], rob0.data_bt_ex[12], rob0.data_ba_ex[12], rob0.cb_npc.data[12], rob0.cb_pdest.data[12], rob0.cb_bt_pd.data[12], rob0.cb_ba_pd.data[12]);
+    $display("Entry 13 |  %b\t%b\t%0d\t%0d\t%0d\t%b\t%0d",rob0.data_rdy[13], rob0.data_bt_ex[13], rob0.data_ba_ex[13], rob0.cb_npc.data[13], rob0.cb_pdest.data[13], rob0.cb_bt_pd.data[13], rob0.cb_ba_pd.data[13]);
+    $display("Entry 14 |  %b\t%b\t%0d\t%0d\t%0d\t%b\t%0d",rob0.data_rdy[14], rob0.data_bt_ex[14], rob0.data_ba_ex[14], rob0.cb_npc.data[14], rob0.cb_pdest.data[14], rob0.cb_bt_pd.data[14], rob0.cb_ba_pd.data[14]);
+    $display("Entry 15 |  %b\t%b\t%0d\t%0d\t%0d\t%b\t%0d",rob0.data_rdy[15], rob0.data_bt_ex[15], rob0.data_ba_ex[15], rob0.cb_npc.data[15], rob0.cb_pdest.data[15], rob0.cb_bt_pd.data[15], rob0.cb_ba_pd.data[15]);
 
-    $display("==========================\n");
+		$display("==============================================================\n");
 	  end
 	endtask
 
 	task reset_all;
 	  begin
 			count = 0;
-			move_tail=0;
-  		din1_en=0;
-  		din2_en=0;
-  		dout1_req=0;
-  		dout2_req=0;
-  		tail_new=0; 
-			din1=0;
-  		din2=0;
+			NPC = 0;
+			reset = 0; din1_req = 0; din2_req = 0; dup1_req = 0; dup2_req = 0;
+	    ir_in1 = 0; ir_in2 = 0;
+	    npc_in1 = 0; npc_in2 = 0;
+	    pdest_in1 = 0; pdest_in2 = 0;
+	    bt_pd_in1 = 0; bt_pd_in2 = 0;
+	    ba_pd_in1 = 0; ba_pd_in2 = 0;
+	    bt_ex_in1 = 0; bt_ex_in2 = 0;
+	    ba_ex_in1 = 0; ba_ex_in2 = 0;
+	    rob_idx_in1 = 0; rob_idx_in2 = 0;
   	end
   endtask
 
-	task insert_data;
-			input [1:0] numData;
-			input [`CB_WIDTH-1:0] data1;
-			input	[`CB_WIDTH-1:0] data2;
-		begin	
-      din1=data1;
-			din2=data2;
-      if (numData == 2)
-				begin
-					din1_en=1;
-  				din2_en=1;
-    			$display("### Inserting 2 data: %d, %d\n",data1, data2);
-				end
-			else if(numData == 1)
-				begin
-					din1_en=1;
-					din2_en=0;
-    			$display("### Inserting 1 data: %d\n",data1);
-   			end			
-			else if(numData == 0)
-				begin
-					din1_en=0;
-					din2_en=0;
-  			end			
- 		end
-	endtask
- 
 
-	task remove_data;
-			input [1:0] numData;
-		begin	
 
-      if (numData == 2)
-				begin
-      		dout1_req=1;
-  				dout2_req=1;
-				end
-			else if(numData == 1)
-				begin
-      		dout1_req=1;
-  				dout2_req=0;
-   			end			
-			else if(numData == 0)
-				begin
-					dout1_req=0;
-  				dout2_req=0;
-  			end			
- 		end
-	endtask
+  // Task to allocate an instruction 
+  task allocate_inst;
+	input [1:0] num_inst;
+  input [`PRF_IDX-1:0] dest_idx1, dest_idx2;
+	input bt_pd1, bt_pd2, isbranch1, isbranch2;
+	input [63:0] ba_pd1, ba_pd2; 
+  begin
 
-	task moveTail;
-			input move;
-			input	[`CB_IDX-1:0] offset;
-		begin	
-			move_tail=move;	
-			tail_new=offset;
-		end	
-	endtask
- 
+    if (num_inst >= 1) begin
+			din1_req = 1;
+			
+			NPC = NPC + 1;
+			npc_in1 = NPC;  // arbitrary
+			ir_in1 = NPC/2; // arbitrary
+			pdest_in1 = dest_idx1;
+			bt_pd_in1 = bt_pd1;
+			ba_pd_in1 = ba_pd1; 
+			isbranch_in1 = isbranch1;
+			$display("Allocating Inst @%4.0fns: PRF=%d, ISBR=%b, BT:%b, BA:%d\n",	$time, dest_idx1, isbranch1, bt_pd1, ba_pd1);
+
+			if (num_inst == 2) begin
+				din2_req = 1;
+				
+				NPC = NPC + 1;
+				npc_in2 = NPC;  // arbitrary
+				ir_in2 = NPC/2; // arbitrary
+				pdest_in2 = dest_idx2;
+				bt_pd_in2 = bt_pd2;
+				ba_pd_in2 = ba_pd2; 
+				isbranch_in2 = isbranch2;
+				$display("Allocating Inst @%4.0fns: PRF=%d, ISBR=%b, BT:%b, BA:%d\n",	$time, dest_idx2, isbranch2, bt_pd2, ba_pd2);
+			end
+
+		end else begin
+			din1_req = 0;
+			din2_req = 0;
+		end
+		
+  end
+  endtask
+
+  // Task to update an instruction 
+  task up_inst;
+	input [1:0] num_inst;
+	input [`ROB_IDX-1:0] rob1, rob2;
+	input bt_ex1, bt_ex2;
+	input [63:0] ba_ex1, ba_ex2; 
+  begin
+
+    if (num_inst >= 1) begin
+			dup1_req = 1;
+			
+			bt_ex_in1 = bt_ex1;
+			ba_ex_in1 = ba_ex1;
+			$display("Updating ROB#%0d @%4.0fns: BT=%b, BA=%d\n",	rob1, $time, bt_ex1, ba_ex1);
+
+			if (num_inst == 2) begin
+				dup2_req = 1;
+				
+				bt_ex_in1 = bt_ex1;
+				ba_ex_in1 = ba_ex1;
+				$display("Updating ROB#%0d @%4.0fns: BT=%b, BA=%d\n",	rob2, $time, bt_ex2, ba_ex2);
+			end
+
+		end else begin
+			dup1_req = 0;
+			dup2_req = 0;
+		end
+		
+  end
+  endtask
 
 	initial
 	  begin
     clk = 1'b0;
-    // Reset CB
-    reset = 1'b1;      // Assert Reset
-    @(negedge clk);
-    reset = 1'b0;      // Deassert Reset
+    // Reset ROB
+    reset = 1'b1;@(negedge clk); reset = 1'b0; 
+
     // Initialize input signals
     reset_all();
   
     @(negedge clk);
 		
-    // Test case #1: Insert items 
-    $display("=============================================================\n");
-    $display("@@@ Test case #1: Insert and remove one at a time \n");
+    $display("=============================================================");
+    $display("@@@ Test case #1: Insert & Remove one at a time");
     $display("=============================================================\n");
     
     $display("============[        INSERT       ]==========================\n");
-    insert_data(2,3,0);
-		show_IO_content();
-		// insert two at a time
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-		insert_data(0,0,0);
+		show_io();
+		// insert one at a time
+    @(negedge clk);show_contents();show_io();
 
     $display("============[        REMOVE       ]==========================\n");
     
-    remove_data(2);
-		show_IO_content();
+		show_io();
 		// remove two at a time
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-		remove_data(0);
+    @(negedge clk);show_contents();show_io();
+    @(negedge clk);show_contents();show_io();
+    @(negedge clk);show_contents();show_io();
+    @(negedge clk);show_contents();show_io();
+    @(negedge clk);show_contents();show_io();
 
 
 
 		// Test case #2: Pull items
-    $display("=============================================================\n");
-    $display("@@@ Test case #2: Insert and remove two at a time\n");
+    $display("=============================================================");
+    $display("@@@ Test case #2: Insert and remove two at a time");
     $display("=============================================================\n");
 
-		// insert one at a time
-    // Reset CB
-    reset = 1'b1;      // Assert Reset
-    @(negedge clk);
-    reset = 1'b0;      // Deassert Reset
-    @(negedge clk);
-		
-		show_entry_content();show_IO_content();
-    $display("============[        INSERT       ]==========================\n");
-    insert_data(1,5,0);
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-		insert_data(0,0,0);
-    
-    $display("============[        REMOVE       ]==========================\n");	
-		show_entry_content();show_IO_content();
-    remove_data(1);
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-    @(negedge clk);show_entry_content();show_IO_content();
-		remove_data(0);
 
 		// Test case #3: Insert & pull items at the same time 
-    $display("=============================================================\n");
-    $display("@@@ Test case #3: Insert & Pull test\n");
+    $display("=============================================================");
+    $display("@@@ Test case #3: Insert & Pull test");
     $display("=============================================================\n");
     
-    // Reset CB
-    reset = 1'b1;      // Assert Reset
-		reset_all();
-    @(negedge clk);
-    reset = 1'b0;      // Deassert Reset
-	  @(negedge clk);
-    
-
-    insert_data(2,1,2);
-    @(negedge clk);show_entry_content();show_IO_content();
-		insert_data(2,3,4);
-		remove_data(2);
-		@(negedge clk);show_entry_content();show_IO_content();
-		insert_data(2,5,6);
-		remove_data(2);
-		@(negedge clk);show_entry_content();show_IO_content();
-		insert_data(2,7,8);
-		remove_data(2);
-		@(negedge clk);show_entry_content();show_IO_content();
-		insert_data(2,9,10);
-		remove_data(2);
-		@(negedge clk);show_entry_content();show_IO_content();
-		insert_data(0,0,0);
-		remove_data(1);
-		@(negedge clk);show_entry_content();show_IO_content();
-		@(negedge clk);show_entry_content();show_IO_content();
-		
 
 
-		// Test case #4: Manually move tail position
-    $display("=============================================================\n");
-    $display("@@@ Test case #4: Move tail test\n");
-    $display("=============================================================\n");
-		// Reset CB
-    reset = 1'b1;      // Assert Reset
-    reset_all();    
-		@(negedge clk);
-    reset = 1'b0;      // Deassert Reset
-    @(negedge clk);
-
-		insert_data(1,20,0);
-    @(negedge clk);show_entry_content();show_IO_content();
-		@(negedge clk);show_entry_content();show_IO_content();
-		insert_data(2,5,0);
-		remove_data(1);
-		@(negedge clk);show_entry_content();show_IO_content();
-		remove_data(2);
-		@(negedge clk);show_entry_content();show_IO_content();
-		insert_data(0,0,0);
-		moveTail(1,3);
-    $display("Moving tail\n"); 
-		@(negedge clk);show_entry_content();show_IO_content();
-		moveTail(1,1);
-		@(negedge clk);show_entry_content();show_IO_content();
-		remove_data(0);
-		moveTail(0,0);
-		insert_data(2,3,4);
-		@(negedge clk);show_entry_content();show_IO_content();
 
     $display("All Testcase Passed!\n"); 
     $finish; 
--- verilog/rob.v
+++ verilog/rob.v
@@ -4,9 +4,9 @@ module rob (clk, reset,
 						// allocate requests
 						din1_req, din2_req,
 						// update requests
-						up1_req, up2_req,
+						dup1_req, dup2_req,
 						// incoming values
-						ir_in1, ir_in2, npc_in1, npc_in2, pdest_in1, pdest_in2, ba_pd_in1, ba_pd_in2, bt_pd_in1, bt_pd_in2,
+						ir_in1, ir_in2, npc_in1, npc_in2, pdest_in1, pdest_in2, ba_pd_in1, ba_pd_in2, bt_pd_in1, bt_pd_in2, isbranch_in1, isbranch_in2,
 						// values that gets updated
 						ba_ex_in1, ba_ex_in2, bt_ex_in1, bt_ex_in2, 
 						// rob indices for updates
@@ -19,13 +19,14 @@ module rob (clk, reset,
 						branch_miss, ba_out
 						);
 
-  input clk, reset, din1_req, din2_req, up1_req, up2_req;
+  input clk, reset, din1_req, din2_req, dup1_req, dup2_req;
 	input [31:0] ir_in1, ir_in2;
 	input [63:0] npc_in1, npc_in2;
 	input [`PRF_IDX-1:0] pdest_in1, pdest_in2;
 	input bt_pd_in1, bt_pd_in2;
 	input [63:0] ba_pd_in1, ba_pd_in2;
 	input bt_ex_in1, bt_ex_in2;
+	input isbranch_in1, isbranch_in2;
 	input [63:0] ba_ex_in1, ba_ex_in2;
 	input [`ROB_IDX-1:0] rob_idx_in1, rob_idx_in2;
 	
@@ -63,6 +64,7 @@ module rob (clk, reset,
 
 	wire bt_pd_out1, bt_pd_out2;
 	wire [63:0] ba_pd_out1, ba_pd_out2;
+	wire isbranch_out1, isbranch_out2;
 
   // Data input indicators for outside world
 	assign din1_rdy = !full;
@@ -111,14 +113,14 @@ module rob (clk, reset,
 		branch_miss = 0;
 
 		// deal with branch misses
-		if (retire1) begin
+		if (retire1 && isbranch_out1) begin
 			if ((data_bt_ex[head] != bt_pd_out1) || (data_ba_ex[head] != ba_pd_out1)) begin
 				branch_miss = 1;
 				ba_out = data_ba_ex[head];
 				dout2_valid = 0;	
 				move_tail = 1;
 				tail_new = next_head;
-			end else if (retire2) begin
+			end else if (retire2 && isbranch_out2) begin
 				if ((data_bt_ex[head_p1] != bt_pd_out2) || (data_ba_ex[head_p1] != ba_pd_out2)) begin
 					branch_miss = 1;
 					ba_out = data_ba_ex[head_p1];
@@ -192,11 +194,11 @@ module rob (clk, reset,
 			data_rdy[tail_p1]			<= `SD next_data_rdy2;
 
 			// data updates
-			if (up1_req) begin
+			if (dup1_req) begin
 				data_ba_ex[rob_idx_in1] <= `SD ba_ex_in1;
 				data_bt_ex[rob_idx_in1] 	<= `SD bt_ex_in1;
 				data_rdy[rob_idx_in1] 		<= `SD 1'b1;
-				if (up2_req) begin
+				if (dup2_req) begin
 					data_ba_ex[rob_idx_in2] <= `SD ba_ex_in2;
 					data_bt_ex[rob_idx_in2] 	<= `SD bt_ex_in2;
 					data_rdy[rob_idx_in2] 		<= `SD 1'b1;
@@ -218,38 +220,21 @@ module rob (clk, reset,
 	// ===================================================
 
 	// Circular buffer for IR
-	cb #(.CB_IDX(`ROB_IDX), .CB_WIDTH(32), .CB_LENGTH(`ROB_SZ)) cb_ir (
-		.clk(clk), .reset(reset),	.move_tail(move_tail), .tail_new(tail_new), 
-		.din1_en(din1_req), .din2_en(din2_req), .dout1_req(retire1), .dout2_req(retire2),
-		.din1(ir_in1), .din2(ir_in2), .dout1(ir_out1), .dout2(ir_out2), 
-		.full(), .full_almost(), .head(), .tail());
+	cb #(.CB_IDX(`ROB_IDX), .CB_WIDTH(32), .CB_LENGTH(`ROB_SZ)) cb_ir (.clk(clk), .reset(reset),	.move_tail(move_tail), .tail_new(tail_new), .din1_en(din1_req), .din2_en(din2_req), .dout1_req(retire1), .dout2_req(retire2), .din1(ir_in1), .din2(ir_in2), .dout1(ir_out1), .dout2(ir_out2), .full(), .full_almost(), .head(), .tail());
 
 	// Circular buffer for NPC
-	cb #(.CB_IDX(`ROB_IDX), .CB_WIDTH(64), .CB_LENGTH(`ROB_SZ)) cb_npc (
-		.clk(clk), .reset(reset),	.move_tail(move_tail), .tail_new(tail_new), 
-		.din1_en(din1_req), .din2_en(din2_req), .dout1_req(retire1), .dout2_req(retire2),
-		.din1(npc_in1), .din2(npc_in2), .dout1(npc_out1), .dout2(npc_out2), 
-		.full(), .full_almost(), .head(), .tail());
+	cb #(.CB_IDX(`ROB_IDX), .CB_WIDTH(64), .CB_LENGTH(`ROB_SZ)) cb_npc (.clk(clk), .reset(reset),	.move_tail(move_tail), .tail_new(tail_new), .din1_en(din1_req), .din2_en(din2_req), .dout1_req(retire1), .dout2_req(retire2), .din1(npc_in1), .din2(npc_in2), .dout1(npc_out1), .dout2(npc_out2), .full(), .full_almost(), .head(), .tail());
 
 	// Circular buffer for PDEST_IDX
-	cb #(.CB_IDX(`ROB_IDX), .CB_WIDTH(`PRF_IDX), .CB_LENGTH(`ROB_SZ)) cb_pdest (
-		.clk(clk), .reset(reset),	.move_tail(move_tail), .tail_new(tail_new), 
-		.din1_en(din1_req), .din2_en(din2_req), .dout1_req(retire1), .dout2_req(retire2),
-		.din1(pdest_in1), .din2(pdest_in2), .dout1(pdest_out1), .dout2(pdest_out2), 
-		.full(), .full_almost(), .head(), .tail());
+	cb #(.CB_IDX(`ROB_IDX), .CB_WIDTH(`PRF_IDX), .CB_LENGTH(`ROB_SZ)) cb_pdest (.clk(clk), .reset(reset),	.move_tail(move_tail), .tail_new(tail_new), .din1_en(din1_req), .din2_en(din2_req), .dout1_req(retire1), .dout2_req(retire2),	.din1(pdest_in1), .din2(pdest_in2), .dout1(pdest_out1), .dout2(pdest_out2), .full(), .full_almost(), .head(), .tail());
 
 	// Circular buffer for Predicted Branch Address
-	cb #(.CB_IDX(`ROB_IDX), .CB_WIDTH(64), .CB_LENGTH(`ROB_SZ)) cb_ba_pd (
-		.clk(clk), .reset(reset),	.move_tail(move_tail), .tail_new(tail_new), 
-		.din1_en(din1_req), .din2_en(din2_req), .dout1_req(retire1), .dout2_req(retire2),
-		.din1(ba_pd_in1), .din2(ba_pd_in2), .dout1(ba_pd_out1), .dout2(ba_pd_out2), 
-		.full(), .full_almost(), .head(), .tail());
+	cb #(.CB_IDX(`ROB_IDX), .CB_WIDTH(64), .CB_LENGTH(`ROB_SZ)) cb_ba_pd (.clk(clk), .reset(reset),	.move_tail(move_tail), .tail_new(tail_new), .din1_en(din1_req), .din2_en(din2_req), .dout1_req(retire1), .dout2_req(retire2),	.din1(ba_pd_in1), .din2(ba_pd_in2), .dout1(ba_pd_out1), .dout2(ba_pd_out2), .full(), .full_almost(), .head(), .tail());
 
 	// Circular buffer for Predicted Branch Direction
-	cb #(.CB_IDX(`ROB_IDX), .CB_WIDTH(1), .CB_LENGTH(`ROB_SZ)) cb_bt_pd (
-		.clk(clk), .reset(reset),	.move_tail(move_tail), .tail_new(tail_new), 
-		.din1_en(din1_req), .din2_en(din2_req), .dout1_req(retire1), .dout2_req(retire2),
-		.din1(bt_pd_in1), .din2(bt_pd_in2), .dout1(bt_pd_out1), .dout2(bt_pd_out2), 
-		.full(), .full_almost(), .head(), .tail());
+	cb #(.CB_IDX(`ROB_IDX), .CB_WIDTH(1), .CB_LENGTH(`ROB_SZ)) cb_bt_pd (.clk(clk), .reset(reset),	.move_tail(move_tail), .tail_new(tail_new), .din1_en(din1_req), .din2_en(din2_req), .dout1_req(retire1), .dout2_req(retire2),	.din1(bt_pd_in1), .din2(bt_pd_in2), .dout1(bt_pd_out1), .dout2(bt_pd_out2), .full(), .full_almost(), .head(), .tail());
+
+	// Circular buffer for Branch Instruction Indicator
+	cb #(.CB_IDX(`ROB_IDX), .CB_WIDTH(1), .CB_LENGTH(`ROB_SZ)) cb_isbranch (.clk(clk), .reset(reset),	.move_tail(move_tail), .tail_new(tail_new), .din1_en(din1_req), .din2_en(din2_req), .dout1_req(retire1), .dout2_req(retire2),	.din1(isbranch_in1), .din2(isbranch_in2), .dout1(isbranch_out1), .dout2(isbranch_out2), .full(), .full_almost(), .head(), .tail());
 
 endmodule
