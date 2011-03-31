13401d0d25558868aec40e4a6a0dba2be3ac9528
killogge@gmail.com <killogge@gmail.com@035cc615-df9f-21d4-9745-e54a158c10d1>
2011-03-31 07:47:29 +0000

    Missed some test_progs that weren't deleted and one that wasn't added

---
+++ test_progs/03-mult-stall.s
@@ -0,0 +1,179 @@
+/* MULQ Program to guarentee a rs stall (not yet done) */
+lda $r1, 0x01
+nop
+mulq $r1 , 0x02, $r2
+mulq $r1 , 0x02, $r3
+mulq $r2 , 0x02, $r4
+mulq $r3 , 0x02, $r5
+mulq $r4 , 0x02, $r6
+mulq $r5 , 0x02, $r7
+mulq $r6 , 0x02, $r8
+mulq $r7 , 0x02, $r9
+mulq $r8 , 0x02, $r10
+mulq $r9 , 0x02, $r11
+mulq $r10, 0x02, $r12
+mulq $r11, 0x02, $r13
+mulq $r12, 0x02, $r14
+mulq $r13, 0x02, $r15
+mulq $r14, 0x02, $r16
+mulq $r15, 0x02, $r17
+mulq $r16, 0x02, $r18
+mulq $r17, 0x02, $r19
+mulq $r18, 0x02, $r20
+mulq $r19, 0x02, $r21
+mulq $r20, 0x02, $r22
+mulq $r21, 0x02, $r23
+mulq $r22, 0x02, $r24
+mulq $r23, 0x02, $r25
+mulq $r24, 0x02, $r26
+mulq $r1 , 0x02, $r2
+mulq $r1 , 0x02, $r3
+mulq $r2 , 0x02, $r4
+mulq $r3 , 0x02, $r5
+mulq $r4 , 0x02, $r6
+mulq $r5 , 0x02, $r7
+mulq $r6 , 0x02, $r8
+mulq $r7 , 0x02, $r9
+mulq $r8 , 0x02, $r10
+mulq $r9 , 0x02, $r11
+mulq $r10, 0x02, $r12
+mulq $r11, 0x02, $r13
+mulq $r12, 0x02, $r14
+mulq $r13, 0x02, $r15
+mulq $r14, 0x02, $r16
+mulq $r15, 0x02, $r17
+mulq $r16, 0x02, $r18
+mulq $r17, 0x02, $r19
+mulq $r18, 0x02, $r20
+mulq $r19, 0x02, $r21
+mulq $r20, 0x02, $r22
+mulq $r21, 0x02, $r23
+mulq $r22, 0x02, $r24
+mulq $r23, 0x02, $r25
+mulq $r24, 0x02, $r26
+mulq $r1 , 0x02, $r2
+mulq $r1 , 0x02, $r3
+mulq $r2 , 0x02, $r4
+mulq $r3 , 0x02, $r5
+mulq $r4 , 0x02, $r6
+mulq $r5 , 0x02, $r7
+mulq $r6 , 0x02, $r8
+mulq $r7 , 0x02, $r9
+mulq $r8 , 0x02, $r10
+mulq $r9 , 0x02, $r11
+mulq $r10, 0x02, $r12
+mulq $r11, 0x02, $r13
+mulq $r12, 0x02, $r14
+mulq $r13, 0x02, $r15
+mulq $r14, 0x02, $r16
+mulq $r15, 0x02, $r17
+mulq $r16, 0x02, $r18
+mulq $r17, 0x02, $r19
+mulq $r18, 0x02, $r20
+mulq $r19, 0x02, $r21
+mulq $r20, 0x02, $r22
+mulq $r21, 0x02, $r23
+mulq $r22, 0x02, $r24
+mulq $r23, 0x02, $r25
+mulq $r24, 0x02, $r26
+mulq $r1 , 0x02, $r2
+mulq $r1 , 0x02, $r3
+mulq $r2 , 0x02, $r4
+mulq $r3 , 0x02, $r5
+mulq $r4 , 0x02, $r6
+mulq $r5 , 0x02, $r7
+mulq $r6 , 0x02, $r8
+mulq $r7 , 0x02, $r9
+mulq $r8 , 0x02, $r10
+mulq $r9 , 0x02, $r11
+mulq $r10, 0x02, $r12
+mulq $r11, 0x02, $r13
+mulq $r12, 0x02, $r14
+mulq $r13, 0x02, $r15
+mulq $r14, 0x02, $r16
+mulq $r15, 0x02, $r17
+mulq $r16, 0x02, $r18
+mulq $r17, 0x02, $r19
+mulq $r18, 0x02, $r20
+mulq $r19, 0x02, $r21
+mulq $r20, 0x02, $r22
+mulq $r21, 0x02, $r23
+mulq $r22, 0x02, $r24
+mulq $r23, 0x02, $r25
+mulq $r24, 0x02, $r26
+mulq $r1 , 0x02, $r2
+mulq $r1 , 0x02, $r3
+mulq $r2 , 0x02, $r4
+mulq $r3 , 0x02, $r5
+mulq $r4 , 0x02, $r6
+mulq $r5 , 0x02, $r7
+mulq $r6 , 0x02, $r8
+mulq $r7 , 0x02, $r9
+mulq $r8 , 0x02, $r10
+mulq $r9 , 0x02, $r11
+mulq $r10, 0x02, $r12
+mulq $r11, 0x02, $r13
+mulq $r12, 0x02, $r14
+mulq $r13, 0x02, $r15
+mulq $r14, 0x02, $r16
+mulq $r15, 0x02, $r17
+mulq $r16, 0x02, $r18
+mulq $r17, 0x02, $r19
+mulq $r18, 0x02, $r20
+mulq $r19, 0x02, $r21
+mulq $r20, 0x02, $r22
+mulq $r21, 0x02, $r23
+mulq $r22, 0x02, $r24
+mulq $r23, 0x02, $r25
+mulq $r24, 0x02, $r26
+mulq $r1 , 0x02, $r2
+mulq $r1 , 0x02, $r3
+mulq $r2 , 0x02, $r4
+mulq $r3 , 0x02, $r5
+mulq $r4 , 0x02, $r6
+mulq $r5 , 0x02, $r7
+mulq $r6 , 0x02, $r8
+mulq $r7 , 0x02, $r9
+mulq $r8 , 0x02, $r10
+mulq $r9 , 0x02, $r11
+mulq $r10, 0x02, $r12
+mulq $r11, 0x02, $r13
+mulq $r12, 0x02, $r14
+mulq $r13, 0x02, $r15
+mulq $r14, 0x02, $r16
+mulq $r15, 0x02, $r17
+mulq $r16, 0x02, $r18
+mulq $r17, 0x02, $r19
+mulq $r18, 0x02, $r20
+mulq $r19, 0x02, $r21
+mulq $r20, 0x02, $r22
+mulq $r21, 0x02, $r23
+mulq $r22, 0x02, $r24
+mulq $r23, 0x02, $r25
+mulq $r24, 0x02, $r26
+mulq $r1 , 0x02, $r2
+mulq $r1 , 0x02, $r3
+mulq $r2 , 0x02, $r4
+mulq $r3 , 0x02, $r5
+mulq $r4 , 0x02, $r6
+mulq $r5 , 0x02, $r7
+mulq $r6 , 0x02, $r8
+mulq $r7 , 0x02, $r9
+mulq $r8 , 0x02, $r10
+mulq $r9 , 0x02, $r11
+mulq $r10, 0x02, $r12
+mulq $r11, 0x02, $r13
+mulq $r12, 0x02, $r14
+mulq $r13, 0x02, $r15
+mulq $r14, 0x02, $r16
+mulq $r15, 0x02, $r17
+mulq $r16, 0x02, $r18
+mulq $r17, 0x02, $r19
+mulq $r18, 0x02, $r20
+mulq $r19, 0x02, $r21
+mulq $r20, 0x02, $r22
+mulq $r21, 0x02, $r23
+mulq $r22, 0x02, $r24
+mulq $r23, 0x02, $r25
+mulq $r24, 0x02, $r26
+call_pal 0x555
