0b58fb2812d1221bfe59aa6751e4175cd1a9f990
jimeecs <jimeecs@035cc615-df9f-21d4-9745-e54a158c10d1>
2011-04-11 02:13:44 +0000

    Makefile.br br.tcl

--- testbench/testbench_br.v
+++ testbench/testbench_br.v
@@ -1,17 +1,19 @@
 `timescale 1ns/100ps
 
 // testbench works with length
+`define PR_SZ (1<<`PRED_IDX)
 
 module testbench;
 
   integer count,limbo,idx;  //TEST VARS
-	reg	clk,reset;
-	reg	[`SCALAR-1:0]	ROB_taken, ROB_br_en, ROB_valid;
-	reg	[`SCALAR*64-1:0] ID_NPC, ROB_NPC, ROB_taken_address;
-	wire	[63:0] paddress1, paddress2;
-	wire	ptaken1, ptaken2;
+	reg	clk, reset; 
+	reg		[`SCALAR-1:0]    ROB_taken, ROB_br_en;
+	reg		[`SCALAR*64-1:0] IF_NPC;
+	reg	  [`SCALAR*64-1:0] ROB_NPC, ROB_taken_address;
+	wire	[`SCALAR*64-1:0] paddress;
+	wire	[`SCALAR-1:0]    ptaken;
 
- br_file br0 (clk, reset, ID_NPC, ROB_br_en, ROB_NPC, ROB_taken, ROB_taken_address, ROB_valid, paddress1, ptaken1, paddress2, ptaken2);
+	branch_predictor br0 (clk, reset, IF_NPC, ROB_br_en, ROB_NPC, ROB_taken, ROB_taken_address, paddress, ptaken); //Branch Table
 
   always
   begin
@@ -21,65 +23,86 @@ module testbench;
 
 	task reset_all;
 		begin
-		ID_NPC=0;
+		IF_NPC=0;
 		ROB_br_en=0;
 		ROB_taken=0;
+		ROB_NPC=0;
 		ROB_taken_address=0;
-		ROB_valid=0;
 		end
 	endtask
-	
-	task show_freelist;
-	  begin
-		 `define DISPLAY_FREE(i) \
-    $display(" %2d  |  %2d  |  %2d", i, br0.br_entries[i]., br0.BTB_entries[i]);
+/*
 
-    $display("==================");
-    $display(" IDX | RAT | RRAT   ");
-    $display("==================");
-		for(idx=0; idx<`BR_SZ; idx=idx+1)
+	task show_BP;
+	  begin
+		 `define DISPLAY_BTB(i) \
+    $display(" %2d |	%2d 	|	%2d	|	%2d	|	", i, br0.predictor[i], br0.BTB_npc[i], br0.BTB_addr[i]);
+    $display("=================================================");
+    $display(" IDX |  PRED	|	NPC	|	ADDR	| ");
+		$display("=================================================");
+		for(idx=0; idx<`PR_SZ; idx=idx+1)
 			begin    
-				`DISPLAY_FREE(idx)
+				`DISPLAY_BTB(idx)
 			end
-    $display("==================\n"); 
+    $display("================================================\n"); 
     
 	  end
 	endtask
+
 
   task show_IO_content;
 	  begin
 		
     $display("============================================================================================ ");
-    $display("ID_NPC1  |  ID_NPC2  | predict address1 | predict taken1 | predict address2 | predict taken2");
+    $display("IF_NPC1  |  IF_NPC2  | predict address1 | predict taken1 | predict address2 | predict taken2");
     $display("============================================================================================ ");
 
-    $display(" 0x%h    |  0x%h     |    0x%h  | %b | 0x%h | %b",ID_NPC[`SEL(64,1)], ID_NPC[`SEL(64,2)],paddress1, ptaken1, paddress2, ptaken2);
-    $display("============================================================================================ ");
-	  end
-	endtask
-
-	task show_content;
-	  begin
-		
-    $display("============================================================================================ ");
-    $display("");
-    $display("============================================================================================ ");
-
-    $display(" 0x%h    |  0x%h     |    0x%h  | %b | 0x%h | %b",ID_NPC[`SEL(64,1)], ID_NPC[`SEL(64,2)],paddress1, ptaken1, paddress2, ptaken2);
+    $display(" %2d    | 	 %2d     |   			%2d   	  | 			%b 			 | 				%2d			  | 			%b      ", IF_NPC[`SEL(64,1)], IF_NPC[`SEL(64,2)],paddress[`SEL(64,1)], ptaken[0], paddress[`SEL(64,2)], ptaken[1]);
     $display("============================================================================================ ");
 	  end
 	endtask
 
 
-	task insert_PC;
-			input [`BR_IDX-1:0] pc;
+//insert(BR_en, RB_taken, NPC1, NPC2, RB_NPC1, RB_NPC2, RB_taken address1,RB_taken address2)
+	task insert;
+	input		[`SCALAR-1:0]    RB_br_en, RB_taken ;
+	input		[63:0] NPC1,NPC2, RB_NPC1, RB_NPC2;
+	input	  [63:0] RB_taken_address1,RB_taken_address2 ;
 		begin
+
+		if (RB_br_en > 0) begin
+			ROB_br_en[0] = 1'b1;
+			ROB_br_en[1] = 1'b0;
+			IF_NPC[`SEL(64,1)] = NPC1;
+			ROB_NPC[`SEL(64,1)] = RB_NPC1;
+			ROB_taken_address[`SEL(64,1)] = RB_taken_address1;
+				if(RB_taken[0]) begin
+				ROB_taken[0] =1;
+				end
+		if (RB_br_en == 2) begin
+			ROB_br_en[1] = 1'b1;
+			IF_NPC[`SEL(64,2)] = NPC2;
+			ROB_NPC[`SEL(64,2)] = RB_NPC2;
+			ROB_taken_address[`SEL(64,2)] = RB_taken_address2;
+				if(RB_taken[1]) begin
+				ROB_taken[1] =1;
+				end
+	  end
+		end else begin
+			ROB_br_en[0] = 1'b0;
+			ROB_br_en[1] = 1'b0;
+		end
 		end
 	endtask
-
-
+*/
 initial
 begin
+	clk = 1'b0;
+	reset_all();
+	@(negedge clk);
+	$display("============================================================================================ ");
+  $display(" TEST1: RANDOM INSERT  ");
+  $display("============================================================================================ ");
+
 $finish;
 end
 		
